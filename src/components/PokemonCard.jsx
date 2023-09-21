db5765ed6c2f6d11c1f0a81161cf9f3d3267ab9e
S.K.V.666 <melvin.rossignoles@gmail.com>
2023-09-21 10:52:52 +0200

    added proptypes

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,22 @@
+import PropTypes from "prop-types";
+
 function PokemonCard(props) {
-    console.log(props)
+  console.log(props);
+
+  PokemonCard.propTypes = {
+    pokemon: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      imgSrc: PropTypes.string,
+    }).isRequired,
+  };
 
   return (
     <figure>
-      {props.pokemon.imgSrc ? <img src={props.pokemon.imgSrc} alt={props.pokemon.name} /> : <p>???</p>}
+      {props.pokemon.imgSrc ? (
+        <img src={props.pokemon.imgSrc} alt={props.pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
       <figcaption>
         <p>{props.pokemon.name}</p>
       </figcaption>
