c19dceaee5eff29bc690479686a8733657455223
S.K.V.666 <melvin.rossignoles@gmail.com>
2023-09-21 11:47:08 +0200

    added useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -2,45 +2,60 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 
 function App() {
   const pokemonList = [
     {
-        name: "bulbasaur",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-      },
-      {
-        name: "charmander",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-      },
-      {
-        name: "squirtle",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-      },
-      {
-        name: "pikachu",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-      },
-      {
-        name: "mew",
-      },
-    ];
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
   const [pokemonIndex, setPokemonIndex] = useState(0);
   const increment = () => setPokemonIndex(pokemonIndex + 1);
   const decrement = () => setPokemonIndex(pokemonIndex - 1);
 
+  useEffect(() => {
+    alert("hello pokemon trainer :)");
+    if (pokemonList[pokemonIndex].name === "pikachu") {
+      alert("pika pikachu !!!");
+    }
+  }, []);
+
+  useEffect(() => {
+    pokemonList[pokemonIndex].name === "pikachu"
+      ? alert("pika pikachu !!!")
+      : "";
+  });
+
   return (
     <>
       <div>
         <Navbar increment={increment} decrement={decrement} />
-        <PokemonCard pokemon={pokemonList[pokemonIndex] ? pokemonList[pokemonIndex] : "" } />
+        <PokemonCard
+          pokemon={pokemonList[pokemonIndex] ? pokemonList[pokemonIndex] : ""}
+        />
       </div>
     </>
   );
